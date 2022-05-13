9f4b7b2e4f9e63e8cc95bf5502de800dcf2c0933
hermes7144 <kichancho@gmail.com>
2022-05-14 01:17:29 +0900

    redux 리팩토링

--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -14,8 +14,17 @@ const SampleContainer = ({
 }) => {
   // 클래스 형태의 컴포넌트였다면 componentDidMount
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+    // 그 내부에서 async 함수를 선언하고 호출해 줍니다.
+    const fn = async () => {
+     try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+       console.log(e);
+      }
+    }
+    fn();
   }, [getPost, getUsers]);
   return (
     <Sample 
@@ -27,14 +36,12 @@ const SampleContainer = ({
   )
 }
 
-
-
 export default connect(
-  ({ sample}) => ({
+  ({ sample, loading}) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS,
+    loadingPost: loading.GET_POST,
+    loadingUsers: loading.GET_USERS,
   }),
   {
     getPost,
